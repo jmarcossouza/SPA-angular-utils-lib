ffe69976df75174330e553d7aab58bd12a6e0a9a
João Marcos A. Souza <contato@jmarcossouza.com>
2020-07-28 23:31:25 -0300

    comentando classes de utils

--- projects/angular-utils/src/lib/class/ReactiveFormsUtils.ts
+++ projects/angular-utils/src/lib/class/ReactiveFormsUtils.ts
@@ -1,7 +1,15 @@
 import { FormControl, AbstractControl } from '@angular/forms';
 
+/**
+ * Métodos auxiliares do Reactive Forms, métodos para [FormControl](https://angular.io/api/forms/FormControl),
+ * [FormGroup](https://angular.io/api/forms/FormGroup) e etc. Todos os métodos são estáticos.
+ */
 export class ReactiveFormsUtils {
 
+    /**
+     * Indica se o FormControl tem o [Validator](https://angular.io/api/forms/Validators) required
+     * @param formControl FormControl a ser analisado
+     */
     public static hasRequiredValidator(formControl: FormControl): boolean {
         if (formControl.validator) { // Verificar se há algum validator antes
             // Se existir o required vai retornar um objeto { required: true }
